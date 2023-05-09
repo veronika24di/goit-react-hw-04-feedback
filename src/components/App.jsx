81c0c1484a590884e46f968261d008481adf7bfe
veronika24di <veronikanof909@gmail.com>
2023-05-09 18:01:25 +0300

    hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 import FeedbackOptions from './Feedback/FeedbackButton';
-import Statistics from './Feedback/FeedbackValue';
+import Statistics from './Statistics/Statistics';
 import styles from './Feedback/Feedback.module.css';
 
-class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [total, setTotal] = useState(0);
+  const [percentage, setPercentage] = useState(0);
+
+  const onLeaveFeedback = el => {
+    setState(prev => ({ ...prev, [el]: prev[el] + 1 }));
   };
 
-  onLeaveFeedback = el => {
-    this.setState(prevState => {
-      return {
-        [el]: prevState[el] + 1,
-      };
-    });
-  };
-  countTotalFeedback = () =>
-    Object.values(this.state).reduce((acc, el) => (acc += el), 0);
-  countPositiveFeedbackPercentage = () =>
-    (this.state.good * 100) / this.countTotalFeedback();
+  useEffect(() => {
+    const totalValue = Object.values(state).reduce((acc, el) => (acc += el), 0);
+    setTotal(totalValue);
+    setPercentage((state.good * 100) / totalValue);
+  }, [state]);
 
-  render() {
-    const { good, neutral, bad } = this.state;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 25,
+        color: '#010101',
+        padding: 25,
+      }}
+    >
+      <div className={styles.feedback}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeavefeedback={onLeaveFeedback}
+          />
+        </Section>
 
-    return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 25,
-          color: '#010101',
-          padding: 25,
-        }}
-      >
-        <div className={styles.feedback}>
-          <Section title="Please leave feedback">
-            <FeedbackOptions
-              options={['good', 'neutral', 'bad']}
-              onLeavefeedback={this.onLeaveFeedback}
+        {total === 0 ? (
+          <Section>
+            <Notification message="There is no feedback" />
+          </Section>
+        ) : (
+          <Section>
+            <Statistics
+              state={state}
+              total={total}
+              positivePercentage={percentage}
             />
           </Section>
-
-          {this.countTotalFeedback() === 0 ? (
-            <Section>
-              <Notification message="There is no feedback" />
-            </Section>
-          ) : (
-            <Section>
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={this.countTotalFeedback()}
-                positivePercentage={this.countPositiveFeedbackPercentage()}
-              />
-            </Section>
-          )}
-        </div>
+        )}
       </div>
-    );
-  }
-}
-
-export default App;
+    </div>
+  );
+};
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Feedback.module.css';
+import styles from './Statistics.module.css';
 
-export default function Statistics({ good, neutral, bad, total, positivePercentage }) {
-    return (
+export default function Statistics({
+  state: { good, neutral, bad },
+  total,
+  positivePercentage,
+}) {
+  return (
     <div className={styles.wrapperValue}>
       <h2 className={styles.description}>Statistics</h2>
       <ul className={styles.list}>
@@ -11,17 +15,21 @@ export default function Statistics({ good, neutral, bad, total, positivePercenta
         <li className={styles.item}>Neutral: {neutral}</li>
         <li className={styles.item}>Bad: {bad}</li>
         <li className={styles.item}>Total: {total}</li>
-        <li className={styles.item}>Positive feedback: { isNaN(positivePercentage)? 0: `${Math.round(positivePercentage)}%` }</li>
+        <li className={styles.item}>
+          Positive feedback:{' '}
+          {isNaN(positivePercentage) ? 0 : `${Math.round(positivePercentage)}%`}
+        </li>
       </ul>
     </div>
   );
 }
 
-
 Statistics.propTypes = {
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+  state: PropTypes.shape({
     good: PropTypes.number.isRequired,
     bad: PropTypes.number.isRequired,
     neutral: PropTypes.number.isRequired,
-    total: PropTypes.number.isRequired,
-    positivePercentage: PropTypes.number.isRequired,
-}
+  }),
+};
